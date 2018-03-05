30c1fef056d7e4e2928d877163a5c1b6fa7d573c
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-03-05 12:51:48 +1100

    logging added

--- spin_up.rb
+++ spin_up.rb
@@ -1,7 +1,17 @@
+require 'logger'
+
+logger = Logger.new 'scrape.log'
+logger.debug "Starting new scrape"
+
 require 'watir'
 
+alec_welby = "https://www.linkedin.com/in/alec-webley-2b900531/"
 
-def login(br)
+def login(br, logger)
+  logger.debug "login process starting"
+  # fills the email and password fields with some text and clicks the login button
+  # once this is finished, we should have logged into this Linkedin Account
+
   email = br.text_field(:id, "login-email")
   email.set("idof@live.com.au")
 
@@ -11,8 +21,24 @@ def login(br)
   br.element(:id, "login-submit").click()
 end
 
+def goto_next_person(br)
+  logger.debug "finding next profile..."
+  # gathers all the "people also viwed" profiles into a collection, selects a random profile, and navigates to it
+  people = br.elements(:class, ["name", "actor-name"])
 
-BR = Watir::Browser.new :firefox
-BR.goto('https://www.linkedin.com/?originalSubdomain=au')
+  random = people[rand(people.length)]
 
-login(BR)
+end
+
+
+br = Watir::Browser.new :firefox
+br.goto('https://www.linkedin.com/?originalSubdomain=au')
+
+login(br, logger)
+
+logger.debug "going to Alex Welby's profile"
+br.goto(alec_welby)
+
+BR = br
+
+logger.debug "scrape finished sucessfully"
